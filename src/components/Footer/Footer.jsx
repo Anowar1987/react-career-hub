6462fe08751cedb9fd1708137a10cebba16b5881
Anowar1987 <anowarhossen.iim@gmail.com>
2024-03-24 01:34:26 +0600

    Different components created

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,24 +7,30 @@ const Footer = () => {
     <p>ACME Industries Ltd.<br/>Providing reliable tech since 1992</p>
   </aside> 
   <nav>
-    <h6 className="footer-title">Services</h6> 
-    <a className="link link-hover">Branding</a>
-    <a className="link link-hover">Design</a>
-    <a className="link link-hover">Marketing</a>
-    <a className="link link-hover">Advertisement</a>
-  </nav> 
-  <nav>
     <h6 className="footer-title">Company</h6> 
-    <a className="link link-hover">About us</a>
-    <a className="link link-hover">Contact</a>
-    <a className="link link-hover">Jobs</a>
-    <a className="link link-hover">Press kit</a>
+    <a className="link link-hover">About Us</a>
+    <a className="link link-hover">Work</a>
+    <a className="link link-hover">Latest News</a>
+    <a className="link link-hover">Careers</a>
   </nav> 
   <nav>
-    <h6 className="footer-title">Legal</h6> 
-    <a className="link link-hover">Terms of use</a>
-    <a className="link link-hover">Privacy policy</a>
-    <a className="link link-hover">Cookie policy</a>
+    <h6 className="footer-title">Product</h6> 
+    <a className="link link-hover">Prototype</a>
+    <a className="link link-hover">Plans & Planing</a>
+    <a className="link link-hover">Customers</a>
+    <a className="link link-hover">Integrations</a>
+  </nav>
+  <nav>
+    <h6 className="footer-title">Support</h6> 
+    <a className="link link-hover">Help Desk</a>
+    <a className="link link-hover">Sales</a>
+    <a className="link link-hover">Become a Partner</a>
+    <a className="link link-hover">Developers</a>
+  </nav>
+  <nav>
+    <h6 className="footer-title">Contact</h6> 
+    <a className="link link-hover">524 Broadway , NYC</a>
+    <a className="link link-hover">+1 777 - 978 - 5570</a>
   </nav>
 </footer>
     );
---
+++ src/components/Jobs/Jobs.jsx
@@ -0,0 +1,11 @@
+
+
+const Jobs = () => {
+    return (
+        <div>
+            <h2>This is job page</h2>
+        </div>
+    );
+};
+
+export default Jobs;
